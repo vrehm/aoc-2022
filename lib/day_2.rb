d83815667cf406825593f492bc50bb6fc5034e79
Vincent REHM <contact@vincentrehm.fr>
2022-12-02 12:17:03 +0100

    day 2 resolve

--- lib/day_2.rb
+++ lib/day_2.rb
@@ -1,24 +1,37 @@
 require_relative "./aoc"
 
 class Day2 < AOC
+  SCORING1 = {
+    "A X": 4, # R R => 1 + 3
+    "A Y": 8, # R P => 2 + 6
+    "A Z": 3, # R S => 3 + 0
+    "B X": 1, # P R => 1 + 0
+    "B Y": 5, # P P => 2 + 3
+    "B Z": 9, # P S => 3 + 6
+    "C X": 7, # S R => 1 + 6
+    "C Y": 2, # S P => 2 + 0
+    "C Z": 6, # S S => 3 + 3
+  }
+
+  SCORING2 = {
+    "A X": 3, # S => 3 + 0
+    "A Y": 4, # R => 1 + 3
+    "A Z": 8, # P => 2 + 6
+    "B X": 1, # R => 1 + 0
+    "B Y": 5, # P => 2 + 3
+    "B Z": 9, # S => 3 + 6
+    "C X": 2, # P => 2 + 0
+    "C Y": 6, # S => 3 + 3
+    "C Z": 7, # R => 1 + 6
+  }
+
   def solve(part:)
-    elves = convert(lazy_load_data)
-      .map { |elf| elf.reject { |line| line == "\n" }
-      .map { |line| line.gsub("\n", "") }
-      .map(&:to_i) }
-    calories = elves.map(&:sum).force
-    
-    case part
-    when 1
-      calories.max
-    else
-      calories.max(3).sum
-    end
+    read_input_file
+      .map { |round| mark_scoring(round, part) }
+      .reduce(:+)
   end
 
-  private
-
-  def convert(data)
-    elves = data.slice_when { |line| line == "\n" }
+  def mark_scoring(round, scoring = 1)
+    scoring == 1 ? SCORING1[round.to_sym] : SCORING2[round.to_sym]
   end
 end
--- spec/day_2_spec.rb
+++ spec/day_2_spec.rb
@@ -8,11 +8,124 @@ RSpec.describe Day2 do
   let(:part) { 1 }
 
   context "part one" do
+    describe "mapping all scoring possibilities while hinting the strategy" do
+      it "Rock draws with Rock, R R => 1 + 3" do
+        data = "A X"
+        expected = 4
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+      
+      it "Rock loses to Paper, R P => 2 + 6" do
+        data = "A Y"
+        expected = 8
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+      
+      it "Rock defeats Scissors, R S => 3 + 0" do
+        data = "A Z"
+        expected = 3
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+      
+      it "Paper defeats Rock, P R => 1 + 0" do
+        data = "B X"
+        expected = 1
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+      
+      it "Paper draws with Paper, P P => 2 + 3" do
+        data = "B Y"
+        expected = 5
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+      
+      it "Paper loses to Scissors, P S => 3 + 6" do
+        data = "B Z"
+        expected = 9
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+      
+      it "Scissors loses to Rock, S R => 1 + 6" do
+        data = "C X"
+        expected = 7
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+      
+      it "Scissors defeats Paper, S P => 2 + 0" do
+        data = "C Y"
+        expected = 2
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+      
+      it "Scissors draws with Scissors, S S => 3 + 3" do
+        data = "C Z"
+        expected = 6
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+    end
+
     it { is_expected.to eq(15) }
   end
 
   context "part two" do
     let(:part) { 2 }
-    xit { is_expected.to eq(45000) }
+
+    describe "mapping all scoring possibilities with the real strategy" do
+      it "Scissors loses to Rock, S => 3 + 0" do
+        data = "A X"
+        expected = 3
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+
+      it "Rock draws with Rock, R => 1 + 3" do
+        data = "A Y"
+        expected = 4
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+
+      it "Paper defeats Rock, P => 2 + 6" do
+        data = "A Z"
+        expected = 8
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+
+      it "Rock loses to Paper, R => 1 + 0" do
+        data = "B X"
+        expected = 1
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+
+      it "Paper draws with Paper, P => 2 + 3" do
+        data = "B Y"
+        expected = 5
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+
+      it "Scissors defeats Paper, S => 3 + 6" do
+        data = "B Z"
+        expected = 9
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+
+      it "Paper loses to Scissors, P => 2 + 0" do
+        data = "C X"
+        expected = 2
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+
+      it "Scissors draws with Scissors, S => 3 + 3" do
+        data = "C Y"
+        expected = 6
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+
+      it "Rock defeats Scissors, R => 1 + 6" do
+        data = "C Z"
+        expected = 7
+        expect(day.mark_scoring(data, part)).to eq(expected)
+      end
+    end
+
+    it { is_expected.to eq(12) }
   end
 end
